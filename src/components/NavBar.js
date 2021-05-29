700670ee58ba6132683782843587e185ca3a5daf
mmpacker <mmpacker@gmail.com>
2021-05-29 09:37:22 -0400

    add basic styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
-import React from "react"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-  return <h1>NavBar Page!</h1>
+  return (
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+            Matt Packer
+          </NavLink>
+          <NavLink to="/post"  activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+            Blog Posts
+          </NavLink>
+          <NavLink to="/project" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+            Projects
+          </NavLink>
+          <NavLink to="/about" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+            About Me
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
 }
