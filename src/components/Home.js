e1dac8685b2c407dfb480417d9c85eb5d9b500e3
mmpacker <mmpacker@gmail.com>
2021-05-29 09:59:31 -0400

    stub up home page component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react"
+import image from "../cool-lt_rev-white-orange-background.png"
 
 export default function Home() {
-  return <h1>Home Page!</h1>
+  return (
+    <main>
+      <img src={ image } alt="white orange background" className="absolute object-cover w-full h-full"/>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug home-name">Hi! 👋 I'm Matt</h1>
+      </section>
+    </main>
+  )
 }
