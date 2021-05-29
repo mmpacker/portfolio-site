86e0774676c8191e55b2ab22b9ca0329bf0eee4c
mmpacker <mmpacker@gmail.com>
2021-05-29 09:48:10 -0400

    add social icons to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
@@ -19,6 +20,11 @@ export default function NavBar() {
             About Me
           </NavLink>
         </nav>
+        <div className="inline-flex py-3 py-3 my-6">
+          <SocialIcon url="https://www.linkedin.com/in/mmpacker/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+          <SocialIcon url="https://github.com/mmpacker" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+          <SocialIcon url="https://twitter.com/mmpackerdev" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+        </div>
       </div>
     </header>
   )
